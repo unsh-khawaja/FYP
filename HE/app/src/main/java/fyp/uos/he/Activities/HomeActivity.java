0f8c72abc7dae2b9bead49c2962747ad903a62da
unsa aslam <unsa aslam>
2019-08-20 02:49:39 +0500

    home

--- HE/app/src/main/java/fyp/uos/he/Activities/HomeActivity.java
+++ HE/app/src/main/java/fyp/uos/he/Activities/HomeActivity.java
@@ -2,10 +2,13 @@ package fyp.uos.he.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
+
+import com.google.android.material.tabs.TabLayout;
 
 import fyp.uos.he.R;
 
@@ -15,24 +18,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Resources res = getResources(); // Resource object to get Drawables
-        TabHost tabHost = getTabHost(); // The activity TabHost
-        TabHost.TabSpec spec; // Resusable TabSpec for each tab
-        Intent intent; // Reusable Intent for each tab
-
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        intent = new Intent().setClass(this, HomeVisualActivity.class);
-        // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("home").setIndicator("Home",
-                res.getDrawable(R.drawable.two-houses)).setContent(intent);
-        tabHost.addTab(spec);
-
-        // Do the same for the other tabs
-        intent = new Intent().setClass(this, HomeSpeakActivity.class);
-        spec = tabHost.newTabSpec("Speak").setIndicator(
-                "Speak", res.getDrawable(R.drawable.phonenoicon))
-                .setContent(intent);
-        tabHost.addTab(spec);
-        tabHost.setCurrentTab(0);
     }
 }
