b50b09bee266280b796c07b3aaba00417c05acb3
unsh-khawaja <unsaaslam@gmail.com>
2019-08-20 03:06:37 +0500

    home activity

--- HE/app/src/main/java/fyp/uos/he/Activities/HomeActivity.java
+++ HE/app/src/main/java/fyp/uos/he/Activities/HomeActivity.java
@@ -2,21 +2,29 @@ package fyp.uos.he.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.widget.TabHost;
-
-import com.google.android.material.tabs.TabLayout;
+import android.view.View;
+import android.widget.Button;
 
 import fyp.uos.he.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button mVoiceSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        mVoiceSearch=findViewById(R.id.search_voice_btn);
+        mVoiceSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, VoiceSearchActivity.class));
+            }
+        });
     }
 }
---
+++ HE/app/src/main/java/fyp/uos/he/Activities/VoiceSearchActivity.java
@@ -0,0 +1,15 @@
+package fyp.uos.he.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import fyp.uos.he.R;
+
+public class VoiceSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_search);
+    }
+}
